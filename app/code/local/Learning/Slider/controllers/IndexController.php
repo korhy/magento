cd4f9e97624d37f41fb8b34fa81ada1595996e1f
clement boudinel <clementboudinel@yahoo.fr>
2017-05-30 14:17:19 +0200

    initial commit

---
+++ app/code/local/Learning/Slider/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Learning_Slider_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        //die('it works !');
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
