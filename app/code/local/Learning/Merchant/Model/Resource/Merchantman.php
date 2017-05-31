4d093134b7b4ee14d4bea178183d9a1dff555242
clement boudinel <clementboudinel@yahoo.fr>
2017-05-31 15:20:57 +0200

    Slug

--- app/code/local/Learning/Merchant/Model/Resource/Merchantman.php
+++ app/code/local/Learning/Merchant/Model/Resource/Merchantman.php
@@ -9,4 +9,54 @@ class Learning_Merchant_Model_Resource_merchantman extends Mage_Core_Model_Resou
     {
         $this->_init('learning_merchant/merchantman', 'entity_id');
     }
+
+    /**
+     * Perform actions before object save
+     *
+     * @param Varien_Object $object
+     * @return Mage_Core_Model_Resource_Db_Abstract
+     */
+    protected function _beforeSave(Mage_Core_Model_Abstract $merchantman)
+    {
+        $slug = $this->createSlug($merchantman->getName());
+        $verifiedSlug = $this->checkIfSlugExists($slug, $merchantman->getId());
+
+        $merchantman->setSlug($verifiedSlug);
+
+        parent::_beforeSave($merchantman);
+
+        return $this;
+
+
+    }
+
+    protected function createSlug ($name){
+
+
+        $newName = strtolower($name);
+
+        return str_replace(' ', '_', $newName);
+    }
+
+    protected function checkIfSlugExists($slug, $id){
+
+            $table = $this->getMainTable();
+            $readAdapter = $this->_getReadAdapter();
+
+            $sql = $readAdapter->select()->from($table)->where("slug = '".$slug."' ");
+
+            if($id != null) {
+                $sql->where("entity_id <> ".$id);
+            }
+
+            $merchantmans = $readAdapter->fetchAll($sql);
+
+            if(count($merchantmans) > 0){
+                throw new mysqli_sql_exception('Slug already exists');
+            }
+
+            return $slug;
+
+
+    }
 }
